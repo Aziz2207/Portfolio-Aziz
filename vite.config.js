cf7336629e5c8913b6f37816e02c62fc219ffb2f
hidriaziz <aziz.hidri.2000@gmail.com>
2024-12-24 22:27:03 -0500

    viteconfig fixed

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,9 @@ import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Portfolio-Aziz/', 
+  base: '/', // Chemin pour GitHub Pages
   build: {
-    outDir: 'dist', 
-    assetsDir: 'dist/assets',
+    outDir: 'dist', // Répertoire de sortie pour les fichiers transpilés
+    assetsDir: 'assets', // Répertoire des fichiers CSS et JS
   },
 });
